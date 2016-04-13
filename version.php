c8e2ba13643716d782237dc6cd25658b52304a50
valery <valery.fremaux@gmail.com>
2016-04-13 18:34:05 +0200

    fix version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015111414;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014050800;        // Requires this Moodle version
-$plugin->release = '2.8.0 (Build 2015111414)';
+$plugin->version   = 2015101601;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2014042900;        // Requires this Moodle version
+$plugin->release = '2.9.0 (Build 2015101601)';
 $plugin->maturity   = MATURITY_RC;      // The current plugin version (Date: YYYYMMDDXX)
 $plugin->component = 'block_course_ascendants'; // Full name of the plugin (used for diagnostics)
