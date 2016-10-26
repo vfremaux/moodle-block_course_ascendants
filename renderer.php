218f7c8abc2cc37b496765c1e9abd4d8fd0f020a
valery <valery.fremaux@gmail.com>
2016-10-26 23:57:36 +0200

    Automated sync from SVN

--- renderer.php
+++ renderer.php
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Version details
  *
@@ -24,6 +22,8 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright  2012 onwards Valery Fremaux (valery.fremaux@gmail.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
 
 class block_course_ascendants_renderer extends plugin_renderer_base {
 
